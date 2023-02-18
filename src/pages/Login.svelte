03d8dfc069f7e8ce81509fe228f9a209ee6abc7f
boris <boris.lok@cienet.com>
2023-02-18 10:05:28 +0800

    [Feature] Adjust `login` form style.

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -16,16 +16,121 @@
     }
 </script>
 
-<h3>Login</h3>
-<form on:submit={handle_login}>
-    <input bind:value={username} name="username" placeholder="Username" type="text">
-    <br/>
-    <input bind:value={password} name="password" placeholder="Password" type="password">
-    <br/>
-    <button type="submit">Login</button>
-</form>
+<div class="login-wrapper">
+    <div class="login-container">
+        <div class="label">Login</div>
+        <form on:submit={handle_login}>
+            <div class="input-container">
+                <input bind:value={username} name="username" placeholder=" " required type="text">
+                <span>Username</span>
+            </div>
+            <div class="input-container">
+                <input bind:value={password} class="input" name="password" placeholder=" " required type="password">
+                <span>Password</span>
+            </div>
+            <div class="btn-container">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 
 <style lang="scss">
+  @import "src/assets/constants";
 
+  .login-wrapper {
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-container {
+    width: 80%;
+    max-width: 20rem;
+    height: 20rem;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+
+    box-shadow: 0 0 24px 0 $grey-shadow;
+
+    border-radius: .5rem;
+
+    > .label {
+      font-size: 1.25em;
+      font-weight: 700;
+      margin: 0.75rem 0;
+      width: 100%;
+      text-align: center;
+    }
+
+    > form {
+      width: 100%;
+      padding: 1rem 0.75rem;
+    }
+  }
+
+  .input-container {
+    position: relative;
+    width: 100%;
+
+    margin: 1em 0;
+
+    > input {
+      width: 100%;
+      border: 1px solid $black;
+      padding: 10px;
+      border-radius: 5px;
+      outline: none;
+      font-size: 1em;
+    }
+
+    > span {
+      position: absolute;
+      left: 0;
+      padding: 10px;
+      pointer-events: none;
+      font-size: 1em;
+      text-transform: uppercase;
+      transition: 0.3s;
+    }
+
+    > input:focus ~ span, input:not(:placeholder-shown) ~ span {
+      color: $deep-blue;
+      transform: translateX(10px) translateY(-10px);
+      font-size: 0.65em;
+      padding: 0 10px;
+      background-color: rgb(255, 255, 255);
+    }
+
+    > input:focus {
+      border: 1px solid $deep-blue;
+    }
+  }
+
+  .btn-container {
+    width: 100%;
+
+    text-align: center;
+
+    > button {
+      width: 120px;
+      height: 36px;
+      color: $black;
+      font-size: 1em;
+      cursor: pointer;
+      border: 1px solid $black;
+      border-radius: 5px;
+      background-color: $light-blue;
+    }
+  }
 </style>
